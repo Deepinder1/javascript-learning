ad1142d45ea07d552f5b1f1faa06bad76bc93e11
Deepinder Singh <deepindersingh@Deepinders-MacBook-Air.local>
2020-11-28 17:54:31 +0530

    javascript started

---
+++ home.js
@@ -0,0 +1,25 @@
+console.log('hello');
+// alert('hello this is deepinder');
+
+var b = 'deepinder';
+
+console.log(b);
+
+var someNumber = 45;
+
+console.log(someNumber);
+
+// var age = prompt("what is your age");
+
+// document.getElementById('someText').innerHTML = age;
+
+var num1 = 10;
+num1 /= 10;
+console.log(num1);
+//functions
+//1. creating a function
+function fun(){
+    console.log('this is a funtion');
+}
+//2. calling a function
+fun();
